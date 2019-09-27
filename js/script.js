c63dd3d22fafe5b3d6dd8f0b6bf7aaffa50e3054
nyasha chikari <marshall.chikari@gmail.com>
2019-09-27 23:16:17 +0200

    yes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-window.onscroll = function() {myFunction()};
-
-var header = document.getElementById("myHeader");
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
-
 function menu() {
   var x = document.getElementById("nav-list");
   if (x.style.display === "block") {
