24484b73ac92ef16b16f11256fee23bc85d4d963
nyasha chikari <marshall.chikari@gmail.com>
2019-09-30 02:11:53 +0200

    new look

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function menu() {
-  var x = document.getElementById("nav-list");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
